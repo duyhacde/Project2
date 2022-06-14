a381537381a4ef05f23a36c8de0e1807a225d5e9
QPhuong <phuongquoc247@gmail.com>
2022-06-15 01:11:07 +0700

    Modified api

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -1,14 +1,17 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import axios from 'axios'
 
-//4:18:00
 function UserAPI(token) {
     const [isLogged, setIsLogged] = useState(false)
     const [isAdmin, setIsAdmin] = useState(false)
-
-    useEffect(() => {
+    const [cart, setCart] = useState([])
+    const [history, setHistory] = useState([])
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
+    const [id,setID]=useState('')
+    useEffect(() =>{
         if(token){
-            const getUser = async () => {
+            const getUser = async () =>{
                 try {
                     const res = await axios.get('/user/infor', {
                         headers: {Authorization: token}
@@ -16,17 +19,51 @@ function UserAPI(token) {
 
                     setIsLogged(true)
                     res.data.role === 1 ? setIsAdmin(true) : setIsAdmin(false)
-                }catch(err){
-                    alert(err.response.data.message)
+
+                    setCart(res.data.cart)
+                    setName(res.data.name);
+                    setEmail(res.data.email);
+                    setID(res.data._id)
+                } catch (err) {
+                    alert(err.response.data.msg)
                 }
             }
 
             getUser()
+            
         }
-    }, [token])
+    },[token])
+
+    
+
+    const addCart = async (product) => {
+        if(!isLogged) return alert("Please login to continue buying")
+
+        const check = cart.every(item =>{
+            return item._id !== product._id
+        })
+
+        if(check){
+            setCart([...cart, {...product, quantity: 1}])
+
+            await axios.patch('/user/addcart', {cart: [...cart, {...product, quantity: 1}]}, {
+                headers: {Authorization: token}
+            })
+
+        }else{
+            alert("This product has been added to cart.")
+        }
+    }
+
     return {
         isLogged: [isLogged, setIsLogged],
-        isAdmin: [isAdmin, setIsAdmin]
+        isAdmin: [isAdmin, setIsAdmin],
+        cart: [cart, setCart],
+        addCart: addCart,
+        history: [history, setHistory],
+        name:[name,setName],
+        email:[email,setEmail],
+        id:[id,setID]
     }
 }
 
