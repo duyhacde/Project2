879eaa372cc152776bfe003c241a249bf5b824f6
QPhuong <phuongquoc247@gmail.com>
2022-06-15 00:11:30 +0700

    Add footer page

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="row-info">
+                <div className="kc">
+                    <div className="kc-col-container">
+                        <div className="kc-image">
+                            <img src="https://www.pizzaexpress.vn/wp-content/uploads/2018/05/footer_01.png" className="lazyloaded" alt="Chất lượng dẫn đầu" data-lazy-src="https://www.pizzaexpress.vn/wp-content/uploads/2018/05/footer_01.png" data-was-processed="true"/>
+                        </div>
+                        <div className="kc-title">
+                            <h3>Chất lượng dẫn đầu</h3>
+                        </div>
+                        <div className="kc-description">
+                            <p>Chú trọng khâu tuyển chọn đội ngũ đầu bếp chuyên nghiệp, thực đơn của Pizza Hust luôn được đổi mới, đa dạng với pizza nhiều hương vị, sandwich, mỳ ý và các món ăn nhanh khác.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="kc">
+                    <div className="kc-col-container">
+                        <div className="kc-image">
+                            <img src="https://www.pizzaexpress.vn/wp-content/uploads/2018/05/footer_02.png" className="lazyloaded" alt="Chất lượng dẫn đầu" data-lazy-src="https://www.pizzaexpress.vn/wp-content/uploads/2018/05/footer_01.png" data-was-processed="true"/>
+                        </div>
+                        <div className="kc-title ">
+                            <h3>Giao hàng đúng giờ</h3>
+                        </div>
+                        <div className="kc-description">
+                            <p>Để tăng cường sự tin tưởng và yên tâm với khách hàng, Pizza Hust cam kết luôn giao hàng đúng giờ và chi phí giao hàng rẻ nhất để đảm bảo khách hàng có thể nhận bánh trong thời gian nhanh nhất.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="kc">
+                    <div className="kc-col-container">
+                        <div className="kc-image">
+                            <img src="https://www.pizzaexpress.vn/wp-content/uploads/2018/05/footer_03.png" className="lazyloaded" alt="Chất lượng dẫn đầu" data-lazy-src="https://www.pizzaexpress.vn/wp-content/uploads/2018/05/footer_01.png" data-was-processed="true"/>
+                        </div>
+                        <div className="kc-title ">
+                            <h3>Pizza take-away</h3>
+                        </div>
+                        <div className="kc-description">
+                        <p>Lựa chọn cho mình một hướng đi mới để tạo nên sự khác biệt, mô hình Pizza take away - pizza mang đi giúp khách hàng tiết kiệm thời gian, đem đến sự tiện lợi tối ưu trong việc lựa chọn, thanh toán, đóng gói và nhận hàng.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="kc">
+                    <div className="kc-col-container">
+                        <div className="kc-image">
+                            <img src="https://www.pizzaexpress.vn/wp-content/uploads/2018/05/footer_04.png" className="lazyloaded" alt="Chất lượng dẫn đầu" data-lazy-src="https://www.pizzaexpress.vn/wp-content/uploads/2018/05/footer_01.png" data-was-processed="true"/>
+                        </div>
+                        <div className="kc-title ">
+                            <h3>Phục vụ chuyên nghiệp</h3>
+                        </div>
+                        <div className="kc-description">
+                        <p>Pizza Hust cùng với đội ngũ nhân viên mang đầy sức trẻ và nhiệt huyết, chúng tôi luôn mong muốn đem lại cho khách hàng của mình chất lượng dịch vụ tốt nhất, luôn lắng nghe và chăm sóc những nhu cầu dù là nhỏ nhất của Quý khách.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer
