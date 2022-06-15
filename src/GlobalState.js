949dbe2abd499e408802daeb515dcf0f3e307412
QPhuong <phuongquoc247@gmail.com>
2022-06-15 11:39:01 +0700

    Fix bug change page when login

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -1,26 +1,34 @@
-import axios from 'axios'
 import React, {createContext, useState, useEffect} from 'react'
 import ProductsAPI from './api/ProductsAPI'
 import UserAPI from './api/UserAPI'
 import CategoriesAPI from './api/CategoriesAPI'
+import axios from 'axios'
 
 export const GlobalState = createContext()
 
-export const DataProvider = ({children}) => {
+
+export const DataProvider = ({children}) =>{
     const [token, setToken] = useState(false)
 
-    //4:12:00
-    const refreshToken = async () => {
-        const res = await axios.get('user/refresh_token')
 
-        setToken(res.data.accesstoken)
-    }
+  useEffect(() =>{
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin){
+            const refreshToken = async () =>{
+                const res = await axios.get('/user/refresh_token')
+        
+                setToken(res.data.accesstoken)
+    
+                setTimeout(() => {
+                    refreshToken()
+                }, 10 * 60 * 1000)
+            }
+            refreshToken()
+        }
+    },[])
 
-    useEffect(() => {
-        const firstLogin = localStorage.getItem('first_login')
-        if(firstLogin) refreshToken()
-    }, [])
 
+    
     const state = {
         token: [token, setToken],
         productsAPI: ProductsAPI(),
@@ -28,7 +36,7 @@ export const DataProvider = ({children}) => {
         categoriesAPI: CategoriesAPI()
     }
 
-    return  (
+    return (
         <GlobalState.Provider value={state}>
             {children}
         </GlobalState.Provider>
--- src/components/mainpages/Pages.js
+++ src/components/mainpages/Pages.js
@@ -1,3 +1,4 @@
+      
 import React, {useContext} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Products from './products/Products'
@@ -13,33 +14,34 @@ import CreateProduct from './createProduct/CreateProduct'
 
 import {GlobalState} from '../../GlobalState'
 
+
 function Pages() {
-  const state = useContext(GlobalState)
-  const [isLogged] = state.userAPI.isLogged
-  const [isAdmin] = state.userAPI.isAdmin
+    const state = useContext(GlobalState)
+    const [isLogged] = state.userAPI.isLogged
+    const [isAdmin] = state.userAPI.isAdmin
 
 
-  return (
-      <Routes>
-          <Route path="/" element={<Products/>} />
-          <Route path="/detail/:id" element={<DetailProduct/>} />
+    return (
+        <Routes>
+            <Route path="/" element={<Products/>} />
+            <Route path="/detail/:id" element={<DetailProduct/>} />
 
-          <Route path="/login" element={isLogged ? <NotFound/> : <Login/>} />
-          <Route path="/register" element={isLogged ? <NotFound/> :< Register/>} />
-        
-          <Route path="/category" element={isAdmin ? <Categories/> : <NotFound/>} />
+            <Route path="/login" element={isLogged ? <NotFound/> : <Login/>} />
+            <Route path="/register" element={isLogged ? <NotFound/> :< Register/>} />
+          
+            <Route path="/category" element={isAdmin ? <Categories/> : <NotFound/>} />
 
-          <Route path="/create_product" element={isAdmin ? <CreateProduct/> : <NotFound/>} />
-          <Route path="/edit_product/:id" element={isAdmin ? <CreateProduct/> : <NotFound/>} />
+            <Route path="/create_product" element={isAdmin ? <CreateProduct/> : <NotFound/>} />
+            <Route path="/edit_product/:id" element={isAdmin ? <CreateProduct/> : <NotFound/>} />
 
-          <Route path="/order" element={isLogged ? <OrderHistory/> : <NotFound/>} />
-          <Route path="/order/:id" element={isLogged ? <OrderDetails/> : <NotFound/>} />
+            <Route path="/order" element={isLogged ? <OrderHistory/> : <NotFound/>} />
+            <Route path="/order/:id" element={isLogged ? <OrderDetails/> : <NotFound/>} />
 
-          <Route path="/cart" element={<Cart/>} />
+            <Route path="/cart" element={<Cart/>} />
 
-          <Route path="*" element={<NotFound/>} />
-      </Routes>
-  )
+            <Route path="*" element={<NotFound/>} />
+        </Routes>
+    )
 }
 
 export default Pages
